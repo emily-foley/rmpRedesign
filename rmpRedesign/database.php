91bb32c9ef3e6937f6fadcf1f7fc7227f5bce538
Emily Foley <efoley0823@gmail.com>
2023-03-02 17:23:06 -0500

    changing log to users table

--- rmpRedesign/database.php
+++ rmpRedesign/database.php
@@ -45,7 +45,7 @@
 
 			$password = password_hash($password, PASSWORD_DEFAULT);
 
-			$sql = "INSERT INTO log (email, password, re_password) VALUES ('$email', '$password', '$re_password')";
+			$sql = "INSERT INTO users (email, password, re_password) VALUES ('$email', '$password', '$re_password')";
 
 			mysqli_query($connection, $sql);
 
@@ -71,7 +71,7 @@
 		}
 		if(count($errors) == 0){
 
-			$query1 = "SELECT * FROM log WHERE email='$email'";
+			$query1 = "SELECT * FROM users WHERE email='$email'";
             
             //to find the users input in the database table
 			$result1 = mysqli_query($connection, $query1);
@@ -79,7 +79,7 @@
 			$count1 = mysqli_num_rows($result1);
 
 			if($count1 == 1){ 
-				$query2 = "SELECT password FROM log WHERE email='$email'";
+				$query2 = "SELECT password FROM users WHERE email='$email'";
 
 				$result2 = mysqli_query($connection, $query2);
 
