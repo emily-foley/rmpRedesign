82e445b8f412fd39ba51e4c070ac52dd3e8c0de8
Emily Foley <efoley0823@gmail.com>
2023-03-02 18:06:28 -0500

    removing php to test why broken

--- rmpRedesign/index.php
+++ rmpRedesign/index.php
@@ -1,32 +1,3 @@
-<?php
-
-
-	if ($_SERVER["SERVER_NAME"] == "students.gaim.ucf.edu") {
-		if ($_SERVER["SCRIPT_URL"]=="/~ya818631/dig4172C/rmpRedesign/index.php"){
-		//yara
-		$connection = mysqli_connect('localhost', 'ya818631', '34096885!Yar', 'ya818631');
-		}else {
-		// $connection = mysqli_connect('localhost', 'em248165', '3535A5F4D0EB4F319A17FBEEF735D58Aa!', 'em248165');
-	 	$connection = mysqli_connect('localhost', 'root', '', 'rmpaccount');	
-		}
-	}
-
-    // retrieve all products
-    $sql = "SELECT * FROM professors";
-    $result = $conn->query($sql);
-
-    // retrieve search term
-    $search = $_POST['search'];
-
-    // retrieve filtered results
-    $sql = "SELECT * FROM professors WHERE name LIKE '%".$search."%' OR school LIKE '%".$search."%'";
-    $result = $conn->query($sql);
-
-    // close database connection
-    $conn->close();
-
-  ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -104,12 +75,7 @@
     <button type="submit">Search</button>
     <div class="dropdown">
       <ul>
-        <?php
-        // display filtered results in dropdown menu
-        while ($row = $result->fetch_assoc()) {
-          echo "<li>".$row["name"]."</li>";
-        }
-        ?>
+  
       </ul>
     </div>
   </form>
