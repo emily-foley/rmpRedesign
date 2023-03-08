7d6040ed630b187896ad40229211cf302336341e
Emily Foley <efoley0823@gmail.com>
2023-03-08 16:39:27 -0500

    quotes

--- rmpRedesign/rateprofessor.php
+++ rmpRedesign/rateprofessor.php
@@ -89,7 +89,7 @@ if (isset($_POST['insert'])) {
             <div class="justify-content-left">
               <form action="" method="post">
 
-                <input type="hidden" name="professorID" value=<?php echo $professorID ?>>
+                <input type="hidden" name="professorID" value="<?php echo $professorID ?>">
 
                 <select name="course" id="courses">
                   <option value="DIG4172C">DIG4172C</option>
