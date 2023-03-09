a96f5be2d4d9c7b6eac349d79df32cbcbc2177eb
YaraAnwar <yara.anwar@knights.ucf.edu>
2023-03-09 18:56:26 -0500

    pic changes

--- rmpRedesign/RatePage.php
+++ rmpRedesign/RatePage.php
@@ -207,13 +207,12 @@ $query_run = mysqli_query($connection, $query);
                     <?php
                       $picture = mysqli_query($connection, "SELECT rating FROM ratings WHERE professorID = $professorID"); 
                       $row = mysqli_fetch_assoc($picture); 
-                      if ($row['rating'] == 1 || 2) { 
-                        $rated = echo '<img src="images/Awful.png" alt="Awful"> &nbsp <b>Awful</b>'; 
-                        } elseif ($row['rating'] == 3) 
-                          { $rated = echo '<img src="images/Average.png" alt="Average"> &nbsp <b>Average</b>'; 
-                        } elseif ($row['rating'] == 4 || 5)  
-                          { $rated = echo '<img src="images/Awesome.png" alt="Awesome"> &nbsp <b>Awesome</b>';  } 
-                          echo $rated;                      
+                      if ($row['rating'] == "1 || 2") { 
+                        echo '<img src="images/Awful.png" alt="Awful"> &nbsp <b>Awful</b>'; 
+                        } elseif ($row['rating'] == "3") 
+                          { echo '<img src="images/Average.png" alt="Average"> &nbsp <b>Average</b>'; 
+                        } elseif ($row['rating'] == "4 || 5")  
+                          { echo '<img src="images/Awesome.png" alt="Awesome"> &nbsp <b>Awesome</b>';  }                       
                     ?> 
                     </span>
                   </div>
