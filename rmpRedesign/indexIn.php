723351100775118c9b411ddccc0e3da5e9803b69
YaraAnwar <yara.anwar@knights.ucf.edu>
2023-04-13 20:58:35 -0400

    testing report

--- rmpRedesign/indexIn.php
+++ rmpRedesign/indexIn.php
@@ -35,7 +35,7 @@
     <h4 class="text-center proxima-bold">Select professor to view ratings</h4>
   </div>
 
-  <form method="post" action="RatePageIn.php, reportIn.php">
+  <form id="myForm" method="post" action="RatePageIn.php" >
     <div class="dropdown">
       <ul>
 
@@ -74,6 +74,25 @@
       <input class="fakeBtnBlue" type="submit" name="submit" value="View ratings" />
     </div>
   </form>
+
+    <script>
+      document.getElementById("myForm").addEventListener("submit", function(event) {
+        event.preventDefault(); // prevent the default form submission
+        
+        // get the form data
+        var formData = new FormData(document.getElementById("myForm"));
+        
+        // send the form data to the first page using AJAX
+        var xhr1 = new XMLHttpRequest();
+        xhr1.open("POST", "RatePageIn.php");
+        xhr1.send(formData);
+        
+        // send the form data to the second page using AJAX
+        var xhr2 = new XMLHttpRequest();
+        xhr2.open("POST", "reportIn.php");
+        xhr2.send(formData);
+      });
+    </script>
 
   <div class="row justify-content-center">
     <figure>
