fdf945bcffe54b6c9e96a2cfde731bd1dc95a4b8
YaraAnwar <yara.anwar@knights.ucf.edu>
2023-04-20 23:29:55 -0400

    compare button

--- rmpRedesign/compare2In.php
+++ rmpRedesign/compare2In.php
@@ -63,7 +63,7 @@
 
          ?>
       </ul>
-      <input type="submit" name="submit" value="View ratings" />
+      <input class="fakeBtnBlue" type="submit" name="submit" value="View ratings" />
     </div>
   </form>
 
